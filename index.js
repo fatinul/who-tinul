66c153b2ab92c2e41e9659c62251dab7134370c3
fatinul <fatinulhaziq22@gmail.com>
2025-04-13 22:45:46 +0900

    base-website

---
+++ index.js
@@ -0,0 +1,15 @@
+// index.js
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Serve static files from 'public' folder
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Website running at http://localhost:${PORT}`);
+});
